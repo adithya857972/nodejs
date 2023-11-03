933be7618250bcb14420beb8606c30031d89da9a
root <root@VR27621.linuxvps.com>
2023-11-03 09:31:34 +0100

    added

---
+++ trail.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Replace 'your_folder_id_here' with the actual folder ID
+
+FOLDER_ID="1mfnt5Ke35fiRvQoJic824-8vE1KRZd_z"
+
+# Download all files from the folder
+
+gdown --folder "$FOLDER_ID"
