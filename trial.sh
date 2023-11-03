332f56a90042e8a881ba11467b53f1ce672edf3b
root <root@VR27621.linuxvps.com>
2023-11-03 08:28:03 +0100

    added

--- trial.sh
+++ trial.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
  
 # Set your Google Drive file ID
-FILE_ID="1lTD08vMx1II7hnSF-CCOHXYy01pHAvg6"
+FILE_ID="1SSPbofZh6Ie6qOKxw2-NX4_sqUt4dj_h"
  
 # Set the destination directory
 DOWNLOAD_DIR="/home/root/nodejs/"
  
 # Download the file using gdown
-gdown "https://drive.google.com/file/d/1lTD08vMx1II7hnSF-CCOHXYy01pHAvg6/view?usp=drive_link" -O "$DOWNLOAD_DIR/trail.pdf"
+gdown "https://drive.google.com/file/d/1SSPbofZh6Ie6qOKxw2-NX4_sqUt4dj_h/view" -O "$DOWNLOAD_DIR/trail.zip"
